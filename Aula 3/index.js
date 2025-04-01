a6f15607cdf122f2d804bacd11ebe4d039446496
xToguia <thiagoaugusto142202@gmail.com>
2025-04-01 08:47:27 -0300

    h

--- Aula 3/index.js
+++ Aula 3/index.js
@@ -8,13 +8,17 @@ userService.loadUsers();
 userService.getNextId();
 
 app.post("/users", async(req, res) => {
+try{
     const { nome, email, senha, endereco, telefone, cpf } = req.body;
     if (!nome || !email || !senha || !endereco || !telefone || !cpf) {
         return res.status(400).json({ error: "Todos os campos são obrigatórios: nome, email, senha, cpf, telefone" });
     }
     const user = await userService.addUser(nome, email, senha, endereco, telefone, cpf);
-    res.status(200).json({ user });
-});
+    res.status(201).json(user);
+}catch(erro){
+    console.log(erro);
+    res.status(400).json({error: erro.message});
+}});
 
 // rota pra listar os usuarios
 app.get("/users", (req, res) => {
--- Aula 3/userService.js
+++ Aula 3/userService.js
@@ -41,13 +41,18 @@ class userService {
 
     async addUser(nome, email, senha, endereco, telefone, cpf) {
         try {
+            const cpfexistente = this.users.some(user => user.cpf === cpf);
+            if (cpfexistente) {
+                throw new Error("CPF já cadastrado");
+            }
             const senhaCripto = await bcrypt.hash(senha, 10);
             const user = new User(this.nextId++, nome, email, senhaCripto, endereco, telefone, cpf); // ++ vai adicionar mais 1 no número do id a cada novo usuário, que inicialmente é 1.
             this.users.push(user);
             this.saveUsers();
             return user;
         } catch (erro) {
-            console.log("Erro", erro);
+            console.log("Erro ao adicionar usuário", erro);
+            throw erro;
         }
     }
 
